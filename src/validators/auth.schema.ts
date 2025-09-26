1de55160af0e0ecbdf31dfc69c2a5e2ae4e522a7
YogirajDAhirrao <yogirajahirrao@gmail.com>
2025-09-26 16:21:16 +0530

    fixed

--- src/validators/auth.schema.ts
+++ src/validators/auth.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   name: z.string().min(2),
-  email: z.string().email(),
+  email: z.email(),
   password: z.string().min(6),
 });
 
